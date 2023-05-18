819ef333f4ebb3c3e83e1ca164c2eb1496cf78a9
canusorn <39732595+canusorn@users.noreply.github.com>
2023-05-18 17:04:20 +0700

    add google sheet

--- DHT/DHT.ino
+++ DHT/DHT.ino
@@ -22,6 +22,8 @@ V1 - humid
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <TridentTD_LineNotify.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
 
 // ตั้งค่า
 char ssid[] = "G6PD_2.4G";
@@ -29,12 +31,13 @@ char pass[] = "570610193";
 #define LINE_TOKEN "7m68381D2LS8ByeflY4rVEf9pPEXMXllsuFRNGBTFfG"
 #define TEMP_H 35
 #define HUMID_H 80
+String GSHEET_API = "https://script.google.com/macros/s/AKfycbwnw55UmfUBB3M9ZTPWwmPjoZ1dQ_glG11TgRI94RA980hmTni7dAbR6R782pO46AxS/exec";
 
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
-#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C  
+#define OLED_RESET -1  // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define DHTPIN D7
@@ -93,6 +96,9 @@ void loop() {
     Serial.print(t);
     Serial.println(F("°C"));
 
+    Blynk.virtualWrite(V0, t);
+    Blynk.virtualWrite(V1, h);
+
     // แจ้งเตือนอุณหภูมิผ่านไลน์
     if (t >= TEMP_H) {
       if (!tempNotiState) {
@@ -113,7 +119,52 @@ void loop() {
       humidNotiState = 0;
     }
 
-    Blynk.virtualWrite(V0, t);
-    Blynk.virtualWrite(V1, h);
+    sentToSheet(t, h);
+  }
+}
+
+
+void sentToSheet(float t, float h) {
+
+  if (WiFi.status() != WL_CONNECTED) {
+    return;
+  }
+
+  std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+
+  client->setInsecure();
+
+  HTTPClient https;
+
+  String url = GSHEET_API;
+  url += "?temperature=";
+  url += String(t, 1);
+  url += "&humidity=";
+  url += String(h, 1);
+
+  Serial.print("[HTTPS] begin...\n");
+  if (https.begin(*client, url)) {  // HTTPS
+
+    Serial.print("[HTTPS] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = https.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String payload = https.getString();
+        Serial.println(payload);
+      }
+    } else {
+      Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+    }
+
+    https.end();
+  } else {
+    Serial.printf("[HTTPS] Unable to connect\n");
   }
 }
