87361f453d5461e5a1c5002ed3def0aa33bbf3ee
canusorn <39732595+canusorn@users.noreply.github.com>
2023-05-23 14:41:56 +0700

    add mq2

--- DHT/DHT.ino
+++ DHT/DHT.ino
@@ -9,6 +9,7 @@
 Blynk virtual pin
 V0 - temp
 V1 - humid
+V2 - smoke
 
 การต่อวงจร
 DHT22 -> ESP8266
@@ -20,6 +21,11 @@ oled -> ESP8266
 SCK ->  D1
 SCL ->  D2
 VCC ->  3V
+GND ->  GND
+
+mq2 ->  ESP8266
+A0  ->  A0
+VCC ->  3v
 GND ->  GND
 
 */
@@ -41,12 +47,15 @@ GND ->  GND
 #include <WiFiClientSecureBearSSL.h>
 
 // ตั้งค่า
-char ssid[] = "WIFI_2.4G";
-char pass[] = "PASSWORD";
+char ssid[] = "G6PD_2.4G";
+char pass[] = "570610193";
 #define LINE_TOKEN "7m68381D2LS8ByeflY4rVEf9pPEXMXllsuFRNGBTFfG"
+String GSHEET_API = "https://script.google.com/macros/s/AKfycbwnw55UmfUBB3M9ZTPWwmPjoZ1dQ_glG11TgRI94RA980hmTni7dAbR6R782pO46AxS/exec";
+
+// ตั้งค่าแจ้งเตือนเมื่อมีค่ามากกว่า
 #define TEMP_H 35
 #define HUMID_H 80
-String GSHEET_API = "https://script.google.com/macros/s/AKfycbwnw55UmfUBB3M9ZTPWwmPjoZ1dQ_glG11TgRI94RA980hmTni7dAbR6R782pO46AxS/exec";
+#define SMOKE_H 50
 
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
@@ -57,13 +66,13 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define DHTPIN D7
 
-#define DHTTYPE DHT22  // DHT 11
+#define DHTTYPE DHT11  // DHT 11
 // #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
 DHT dht(DHTPIN, DHTTYPE);
 
 unsigned long previousMillis = 0;
-bool tempNotiState, humidNotiState;
+bool tempNotiState, humidNotiState, smokeNotiState;
 
 void setup() {
 
@@ -94,34 +103,43 @@ void loop() {
 
     float h = dht.readHumidity();
     float t = dht.readTemperature();
+    int smoke = analogRead(A0);
+    smoke = smoke * 100 / 1023;
 
     display.clearDisplay();
     display.setTextSize(2);               // Normal 1:1 pixel scale
     display.setTextColor(SSD1306_WHITE);  // Draw white text
     display.setCursor(0, 0);              // Start at top-left corner
-    display.println(F("DHT sensor"));
-    display.setCursor(0, 25);
+    // display.println(F("DHT sensor"));
+    // display.setTextSize(1);
+    // display.setCursor(0, 25);
     display.println("T : " + String(t, 1) + " C");
     display.println("H : " + String(h, 1) + " %");
+    display.println("S : " + String(smoke) + " %");
     display.display();
 
     Serial.print(F("Humidity: "));
     Serial.print(h);
     Serial.print(F("%  Temperature: "));
     Serial.print(t);
-    Serial.println(F("°C"));
+    Serial.print(F("°C  Smoke: "));
+    Serial.print(smoke);
+    Serial.println(F("%"));
 
     Blynk.virtualWrite(V0, t);
     Blynk.virtualWrite(V1, h);
+    Blynk.virtualWrite(V2, smoke);
 
     // แจ้งเตือนอุณหภูมิผ่านไลน์
     if (t >= TEMP_H) {
       if (!tempNotiState) {
         LINE.notify("อุณหภูมิเกินกำหนด อุณหภูมิขณะนี้ " + String(t) + " C");
         tempNotiState = 1;
+        Blynk.setProperty(V0, "color", "red");
       }
     } else if (tempNotiState) {
       tempNotiState = 0;
+      Blynk.setProperty(V0, "color", "#252757");
     }
 
     // แจ้งเตือนความชื้นผ่านไลน์
@@ -129,17 +147,31 @@ void loop() {
       if (!humidNotiState) {
         LINE.notify("ความชื้นเกินกำหนด ความชื้นขณะนี้ " + String(h) + " %");
         humidNotiState = 1;
+        Blynk.setProperty(V1, "color", "red");
       }
     } else if (humidNotiState) {
       humidNotiState = 0;
+      Blynk.setProperty(V1, "color", "#252757");
     }
 
-    sentToSheet(t, h);
+    // แจ้งเตือนควันผ่านไลน์
+    if (smoke >= SMOKE_H) {
+      if (!smokeNotiState) {
+        LINE.notify("ควันเกินกำหนด ควันขณะนี้ " + String(smoke) + " %");
+        smokeNotiState = 1;
+        Blynk.setProperty(V2, "color", "red");
+      }
+    } else if (smokeNotiState) {
+      smokeNotiState = 0;
+      Blynk.setProperty(V2, "color", "#252757");
+    }
+
+    sentToSheet(t, h, smoke);
   }
 }
 
 
-void sentToSheet(float t, float h) {
+void sentToSheet(float t, float h, int smoke) {
 
   if (WiFi.status() != WL_CONNECTED) {
     return;
@@ -156,6 +188,8 @@ void sentToSheet(float t, float h) {
   url += String(t, 1);
   url += "&humidity=";
   url += String(h, 1);
+  url += "&smoke=";
+  url += String(smoke);
 
   Serial.print("[HTTPS] begin...\n");
   if (https.begin(*client, url)) {  // HTTPS
